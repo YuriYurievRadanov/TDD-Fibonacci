eff71c334eb4223921a1e936231becc4fb6b6ead
YAVUZ KILIÇ <yavuz.kilic@koton.com.tr>
2019-12-05 09:28:07 +0300

    TDD-Fibonacci

---
+++ Fibonacci.Test/FibonacciTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fibonacci.Test
+{
+    public class FibonacciTest
+    {
+        //[Fact]
+        //public void FirstNumberInSequenceIsZero()
+        //{
+        //    Assert.Equal(0, new Fibonacci().GetNumber(0));
+        //}
+
+        //[Fact]
+        //public void SecondNumberInSequenceIsOne()
+        //{
+        //    Assert.Equal(1, new Fibonacci().GetNumber(1));
+        //}
+
+        //[Theory]
+        //[InlineData(0, 0)]
+        //[InlineData(1, 1)]     
+        //public void FirstTwoNumbersAreSameAsIndex(int index, int expected)
+        //{
+        //    Assert.Equal(expected, new Fibonacci().GetNumber(index));
+        //}
+
+        //[Theory]
+        //[InlineData(0, 0)]
+        //[InlineData(1, 1)]
+        //[InlineData(2, 1)]
+        //public void FirstTwoNumbersAreSameAsIndex(int index, int expected)
+        //{
+        //    Assert.Equal(expected, new Fibonacci().GetNumber(index));
+        //}
+
+
+        //[Theory]
+        //[InlineData(0, 0)]
+        //[InlineData(1, 1)]
+        //[InlineData(2, 1)]
+        //[InlineData(3, 2)]
+        //public void FirstTwoNumbersAreSameAsIndex(int index, int expected)
+        //{
+        //    Assert.Equal(expected, new Fibonacci().GetNumber(index));
+        //}
+
+
+        //[Theory]
+        //[InlineData(0, 0)]
+        //[InlineData(1, 1)]
+        //[InlineData(2, 1)]
+        //[InlineData(3, 2)]
+        //[InlineData(4, 3)]
+        //public void FirstTwoNumbersAreSameAsIndex(int index, int expected)
+        //{
+        //    Assert.Equal(expected, new Fibonacci().GetNumber(index));
+        //}
+
+        [Theory]
+        [InlineData(6, 8)]
+        public void FirstTwoNumbersAreSameAsIndex(int index, int expected)
+        {
+            Assert.Equal(expected, new Fibonacci().GetNumber(index));
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        //public int GetNumber(int index)
+        //{
+        //    return 0;
+        //}
+
+        //public int GetNumber(int index)
+        //{
+        //    return index;
+        //}
+
+        //public int GetNumber(int index)
+        //{
+        //    if (index < 2)
+        //        return index;
+        //    return index - 1;
+        //}
+
+        public int GetNumber(int index)
+        {
+            if (index < 2) return index;
+            return GetNumber(index - 1) + GetNumber(index - 2);
+        }
+    }
+}
